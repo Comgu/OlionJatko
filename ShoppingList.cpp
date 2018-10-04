fdf86396e590d14dd3241554af6bbffae8b61f9e
Ville Lehtovirta <villol@live.com>
2018-10-05 01:41:42 +0300

    Added naming for list, string compares now done with helper function from functions.h, new function getContents, new function addObjectsFromRecipe

--- ShoppingList.cpp
+++ ShoppingList.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 
 
+ShoppingList::ShoppingList(const std::string& newName)
+{
+	name = newName;
+}
+
 ShoppingList::ShoppingList()
 {
 }
@@ -9,6 +14,21 @@ ShoppingList::ShoppingList()
 
 ShoppingList::~ShoppingList()
 {
+}
+
+std::string ShoppingList::getName()
+{
+	return name;
+}
+
+void ShoppingList::setName(const std::string& newName)
+{
+	name = newName;
+}
+
+std::vector<DataItem*> ShoppingList::getContents()
+{
+	return objects;
 }
 
 DataItem* ShoppingList::getObject(int i)
@@ -23,7 +43,7 @@ void ShoppingList::addObject(DataItem* newObject)
 	else
 	{
 		int pos = findObjectPos(newObject);
-		if (objects[pos]->getQuantityType() == newObject->getQuantityType())
+		if (compareStrings(objects[pos]->getQuantityType(), newObject->getQuantityType())==true)
 		{
 			objects[pos] = new DataItem(newObject->getName(), newObject->getQuantity() + objects[pos]->getQuantity(), newObject->getQuantityType());
 		}
@@ -32,9 +52,29 @@ void ShoppingList::addObject(DataItem* newObject)
 	}
 }
 
+void ShoppingList::addObjectsFromRecipe(Recipe* recipeObjects)
+{
+	std::vector<DataItem*> tempVector = recipeObjects->getContents();
+	for (unsigned int i = 0; i < tempVector.size(); i++)
+	{
+		if (findIfObjectExistsName(tempVector[i]) == false)
+			objects.push_back(tempVector[i]);
+		else
+		{
+			int pos = findObjectPos(tempVector[i]);
+			if (compareStrings(objects[pos]->getQuantityType(), tempVector[i]->getQuantityType())==true)
+			{
+				objects[pos] = new DataItem(tempVector[i]->getName(), tempVector[i]->getQuantity() + objects[pos]->getQuantity(), tempVector[i]->getQuantityType());
+			}
+			else
+				objects.push_back(tempVector[i]);
+		}
+	}
+}
+
 void ShoppingList::printAllObjects()
 {
-	std::cout << "\nShopping list contents:\n";
+	std::cout << "\n" << name << " contents:\n";
 	for (unsigned int i = 0; i < objects.size(); i++)
 	{
 		std::cout << i + 1 << " " << objects[i]->toString() << "\n";
@@ -45,7 +85,7 @@ int ShoppingList::findObjectPos(DataItem* object)
 {
 	for (unsigned int i = 0; i < objects.size(); i++)
 	{
-		if (object->getName() == objects[i]->getName())
+		if (compareStrings(object->getName(), objects[i]->getName())==true)
 					return i;
 	}
 	return 0;
@@ -55,7 +95,7 @@ bool ShoppingList::findIfObjectExistsName(DataItem* object)
 {
 	for (unsigned int i = 0; i < objects.size(); i++)
 	{
-		if (object->getName() == objects[i]->getName())
+		if (compareStrings(object->getName(), objects[i]->getName())==true)
 					return true;
 	}
 	return false;
--- ShoppingList.h
+++ ShoppingList.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "ItemList.h"
+#include "Recipe.h"
 
 class ShoppingList : public ItemList
 {
 private:
-
+	std::string name;
 public:
+	ShoppingList(const std::string& newName);
 	ShoppingList();
 	~ShoppingList();
+	std::string getName();															// Get & Set
+	void setName(const std::string& newName);
 	DataItem* getObject(int i) override;
+	std::vector<DataItem*> getContents() override;
 	void addObject(DataItem* newObject) override;
+	void addObjectsFromRecipe(Recipe* recipeObjects);
 	void printAllObjects() override;
 	int findObjectPos(DataItem* object);
 	bool findIfObjectExistsName(DataItem* object);
