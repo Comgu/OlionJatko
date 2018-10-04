d1cf441a13dc163de93d7e2ec79d814b2e078ab0
Ville Lehtovirta <villol@live.com>
2018-10-05 01:38:50 +0300

    Added a template function for element count in a vector, added compareString function

--- functions.cpp
+++ functions.cpp
@@ -1,2 +1,13 @@
 #include "functions.h"
 
+bool compareStrings(const std::string str1, const std::string str2)
+{
+	std::string tempStr1 = str1;
+	std::string tempStr2 = str2;
+	std::transform(tempStr1.begin(), tempStr1.end(), tempStr1.begin(), ::tolower);
+	std::transform(tempStr2.begin(), tempStr2.end(), tempStr2.begin(), ::tolower);
+	if (tempStr1 == tempStr2)
+		return true;
+	else
+		return false;
+}
--- functions.h
+++ functions.h
@@ -1,2 +1,16 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "ItemList.h"
 
+// For use in classes derived from ItemList class (ShoppingList, Recipe, DataItemList)
+template<typename T>
+int findListItemCount(const T& list)
+{
+	std::vector<DataItem*> tempVector;
+	tempVector = list->getContents();
+	return tempVector.size();
+}
+
+bool compareStrings(std::string str1, std::string str2);
