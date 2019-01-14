a70258204be0e8f5405ab23735cc05e68edfb3bf
Ville Lehtovirta <villol@live.com>
2019-01-14 20:22:43 +0200

    Modified file names

--- DataItem.h
+++ DataItem.h
@@ -1,24 +1,31 @@
 #pragma once
 #include <string>
+/*! DataItem class
 
+	Class creates item objects with attributes to be used for list classes.
+
+	Attributes: string name, double quantity and string quantityType.
+
+	Methods: Constructors, Destructor, get/setName, get/setQuantityType, get/setQuantity and toString.
+*/
 class DataItem
 {
 private:
-	std::string name;
-	double quantity;
-	std::string quantityType;
+	std::string name;				/*!< Name in string type*/
+	double quantity;				/*!< quantity in double type*/
+	std::string quantityType;		/*!< quantity type in string type, e.g. "gram" or "g"*/
 
-public:															// Constructor & Overload
-	DataItem();
-	DataItem(const std::string& newName);
-	DataItem(const std::string& newName, double newQuantity, const std::string& newQuantityType);
-	~DataItem();	 // Destructor
+public:															
+	DataItem();						/*!< Default constructor*/
+	DataItem(const std::string& newName);																/*!< Constructor with just name*/
+	DataItem(const std::string& newName, double newQuantity, const std::string& newQuantityType);		/*!< Constructor with all attributes*/
+	~DataItem();					/*!< Default destructor*/
 
-	std::string getName();																// Get & Set
-	void setName(const std::string& newName);
-	std::string getQuantityType();
-	void setQuantityType(const std::string& newQuantityType);
-	double getQuantity();
-	void setQuantity(double newQuantity);
-	std::string toString();
+	std::string getName();											/*!< Gets string name from DataItem attributes*/
+	void setName(const std::string& newName);						/*!< Sets a new name for string name*/
+	std::string getQuantityType();									/*!< Gets string quantityType from DataItem attributes*/
+	void setQuantityType(const std::string& newQuantityType);		/*!< Sets a new quantityType for string quantityType*/
+	double getQuantity();											/*!< Gets double quantity from DataItem attributes*/
+	void setQuantity(double newQuantity);							/*!< Sets a new quantity for double quantity*/
+	std::string toString();											/*!< Returns a string format of a class object with its attributes*/
 };
