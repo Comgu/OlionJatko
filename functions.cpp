0153776ec703e2eef9488f96d81b59faf0701436
Ville Lehtovirta <villel_92@hotmail.com>
2019-03-28 18:09:04 +0200

    Added new helper function processItemInput

--- functions.cpp
+++ functions.cpp
@@ -1,4 +1,6 @@
 #include "functions.h"
+#include <iostream>
+#include <cctype>
 
 bool compareStrings(const std::string& str1, const std::string& str2)
 {
@@ -10,4 +12,29 @@ bool compareStrings(const std::string& str1, const std::string& str2)
 		return true;
 	else
 		return false;
+}
+
+
+void processItemInput(std::string& str1, std::string& str2, double& num, std::istream& stream) {
+	std::string tempString = "";
+	bool numberGone = false, endof = false;
+	while (stream.good() == true && endof == false) {							// Needs Error handling (after str2 specifically)
+		stream >> std::ws;
+		int c = std::cin.peek();
+		if (std::isdigit(c) != 0) {
+			stream >> num;
+			numberGone = true;
+			continue;
+		}
+		else if (numberGone == false) {
+			str1.append(" ");
+			stream >> tempString;
+			str1.append(tempString);
+			continue;
+		}
+		else {
+			stream >> str2;
+			endof = true;
+		}
+	}
 }
--- functions.h
+++ functions.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <string>
 #include <algorithm>
+#include <memory>
+#include "dataitem.h"
 
 /*! Contains helper function compareStrings.
 */
 
 bool compareStrings(const std::string& str1, const std::string& str2);		/*!< Compares strings by using tolower. Therefore uppercase characters and lowercase characters are equal. Returns true if strings are equal*/
+
+void processItemInput(std::string& str1, std::string& str2, double& num, std::istream& stream);		// Function that modifies str1, str2 and num values based on the stream input. Used in interface, when user inputs a dataitem using std::cin.
